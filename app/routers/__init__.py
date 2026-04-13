f920288db5dd937de3217ddec465c49279fb828a
Luminescent-Vertigo <kadiebisnathuwi003@gmail.com>
2026-04-13 18:15:34 -0400

    Added income dependency, model, schemas, services, repository, and updated routers init

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -8,10 +8,12 @@ from app.config import get_settings
 
 template_env = Environment(loader = FileSystemLoader("app/templates",), )
 template_env.globals['get_flashed_messages'] = get_flashed_messages
-templates = Jinja2Templates(env=template_env)
+templates = Jinja2Templates(env=template_env) 
+
 static_files = StaticFiles(directory="app/static")
 
 router = APIRouter(tags=["Jinja Based Endpoints"], include_in_schema=get_settings().env.lower() in ["dev","development"])
 api_router = APIRouter(tags=["API Endpoints"], prefix="/api")
 
-from . import (index, login, register, admin_home, user_home, users, logout)
+
+from . import (index, login, register, dashboard, income, expenses, subscription, budget, analytics, admin_home, user_home, users, logout)
