ba6e0e06f3ef1d1bb43c97dadac171008936d464
Luminescent-Vertigo <kadiebisnathuwi003@gmail.com>
2026-04-13 18:58:22 -0400

    Added the analytics and dashboard routes, and updated user_home routes

--- app/routers/user_home.py
+++ app/routers/user_home.py
@@ -5,17 +5,29 @@ from app.dependencies.session import SessionDep
 from app.dependencies.auth import AuthDep, IsUserLoggedIn, get_current_user, is_admin
 from . import router, templates
 
-
-@router.get("/app", response_class=HTMLResponse)
+@router.get("/dashboard", response_class=HTMLResponse)
 async def user_home_view(
     request: Request,
     user: AuthDep,
-    db:SessionDep
+    db: SessionDep
 ):
     return templates.TemplateResponse(
-        request=request, 
-        name="app.html",
+        request=request,
+        name="dashboard.html",
         context={
-            "user": user
+            "request": request,
+            "user": user,
         }
     )
+
+@router.post("/toggle-theme")
+async def toggle_theme(
+    user: AuthDep,
+    db: SessionDep
+):
+    user.theme = "dark" if user.theme == "light" else "light"
+
+    db.add(user)
+    db.commit()
+
+    return {"status": "ok"}
